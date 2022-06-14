5ae759ec1b432f7c6edb62c6e7514adc97f2685c
Bruno Fabre <bruno.hfabre@gmail.com>
2022-06-14 16:40:55 -0300

    change default font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 
-import BarlowBlack from '../assets/fonts/barlow/Barlow-Black.ttf';
-import BarlowBold from '../assets/fonts/barlow/Barlow-Bold.ttf';
-import BarlowExtraBold from '../assets/fonts/barlow/Barlow-ExtraBold.ttf';
-import BarlowExtraLight from '../assets/fonts/barlow/Barlow-ExtraLight.ttf';
-import BarlowLight from '../assets/fonts/barlow/Barlow-Light.ttf';
-import BarlowMedium from '../assets/fonts/barlow/Barlow-Medium.ttf';
-import BarlowRegular from '../assets/fonts/barlow/Barlow-Regular.ttf';
-import BarlowSemiBold from '../assets/fonts/barlow/Barlow-SemiBold.ttf';
-import BarlowThin from '../assets/fonts/barlow/Barlow-Thin.ttf';
-
 export const GlobaStyle = createGlobalStyle`
   :root {
     --rdp-cell-size: 40px;
@@ -30,52 +20,6 @@ export const GlobaStyle = createGlobalStyle`
     border-radius: 24px;
     box-shadow: inset 0 0 10px 10px rgba(0, 0, 0, 0.16);
     border: solid 4px transparent;
-  }
-
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowThin}) format("truetype");
-    font-weight: 100;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowExtraLight}) format("truetype");
-    font-weight: 200;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowLight}) format("truetype");
-    font-weight: 300;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowRegular}) format("truetype");
-    font-weight: 400;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowMedium}) format("truetype");
-    font-weight: 500;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowSemiBold}) format("truetype");
-    font-weight: 600;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowBold}) format("truetype");
-    font-weight: 700;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowExtraBold}) format("truetype");
-    font-weight: 800;
-  }
-  @font-face {
-    font-family: 'Barlow';
-    src: local('Barlow'), url(${BarlowBlack}) format("truetype");
-    font-weight: 900;
   }
 
   * {
@@ -100,7 +44,7 @@ export const GlobaStyle = createGlobalStyle`
   }
 
   body, input, textarea, button {
-    font-family: 'Barlow', sans-serif;
+    font-family: 'Inter', sans-serif;
     font-weight: 400;
   }
 
