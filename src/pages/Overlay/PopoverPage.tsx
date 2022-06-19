5bcb57ce3594f7748f91d291154516a908e6dc8a
Bruno Fabre <bruno.hfabre@gmail.com>
2022-06-18 21:46:03 -0300

    fix popover button text

--- src/pages/Overlay/PopoverPage.tsx
+++ src/pages/Overlay/PopoverPage.tsx
@@ -6,7 +6,7 @@ export function PopoverPage(): JSX.Element {
   return (
     <Flex flex justify="center" align="center">
       <Popover>
-        <Button type="button">Hover to popover</Button>
+        <Button type="button">Click to popover</Button>
 
         <Flex width={240} padding>
           <p>test popover content</p>
