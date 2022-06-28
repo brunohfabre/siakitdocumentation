f4d5b26083e880655d06e0239543c5413ce3f17d
Bruno Fabre <bruno.hfabre@gmail.com>
2022-06-28 11:03:38 -0300

    add card docs

--- src/pages/Layout/CardPage.tsx
+++ src/pages/Layout/CardPage.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
 
 import { Card } from '../../components/Card';
+import { CodeHighlight } from '../../components/CodeHighlight';
 import { Flex } from '../../components/Flex';
+import { Heading } from '../../components/Heading';
+import {
+  SimpleTable,
+  SimpleTableHeader,
+  SimpleTableItem,
+} from '../../components/SimpleTable';
+import { SimpleTableBody } from '../../components/SimpleTable/SimpleTableBody';
+import { Spacer } from '../../components/Spacer';
+import { Text } from '../../components/Text';
+
+const exampleCode = `import { Card } from '@siakit';
+
+<Card />`;
 
 export function CardPage(): JSX.Element {
   return (
-    <Flex padding>
-      <Card>
-        <Flex padding>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
-            nemo quam consequatur, modi sit et dolorum beatae maiores
-            perferendis. Maiores voluptas inventore quisquam at minima vero!
-            Recusandae animi expedita dicta?
-          </p>
-        </Flex>
-      </Card>
+    <Flex flex direction="column" align="center" overflow>
+      <Flex width={768} padding={32} direction="column" gap>
+        <Heading size="xl">Card</Heading>
+        <Card>
+          <Flex height={192} padding justify="center" align="center">
+            <Text>Card component</Text>
+          </Flex>
+        </Card>
+
+        <Spacer height />
+        <Heading>Anatomy</Heading>
+        <CodeHighlight>{exampleCode}</CodeHighlight>
+
+        <Spacer height />
+        <Heading>API reference</Heading>
+        <Heading size="md">Card</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem required>children</SimpleTableItem>
+            <SimpleTableItem>ReactNode</SimpleTableItem>
+            <SimpleTableItem />
+          </SimpleTableBody>
+        </SimpleTable>
+      </Flex>
     </Flex>
   );
 }
