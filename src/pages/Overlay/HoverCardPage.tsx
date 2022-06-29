c255b7e4d82f8b924f3702519a2d4be9c4bba610
= <=>
2022-06-29 17:06:13 -0300

    add popover and hovercard docs

--- src/pages/Overlay/HoverCardPage.tsx
+++ src/pages/Overlay/HoverCardPage.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
 
 import { Button } from '../../components/Button';
+import { Card } from '../../components/Card';
+import { CodeHighlight } from '../../components/CodeHighlight';
 import { Flex } from '../../components/Flex';
+import { Heading } from '../../components/Heading';
 import { HoverCard } from '../../components/HoverCard';
-import { Text } from '../../components/Text';
+import {
+  SimpleTable,
+  SimpleTableHeader,
+  SimpleTableItem,
+} from '../../components/SimpleTable';
+import { SimpleTableBody } from '../../components/SimpleTable/SimpleTableBody';
+import { Spacer } from '../../components/Spacer';
+
+const exampleCode = `import { HoverCard } from '@siakit';
+
+<HoverCard>
+  /* trigger */
+
+  /* content */
+</HoverCard>`;
 
 export function HoverCardPage(): JSX.Element {
   return (
-    <Flex justify="center" align="center">
-      <HoverCard>
-        <Button type="button">hover to card</Button>
+    <Flex flex direction="column" align="center" overflow>
+      <Flex width={768} padding={32} direction="column" gap>
+        <Heading size="xl">HoverCard</Heading>
+        <Card>
+          <Flex height={192} padding justify="center" align="center">
+            <HoverCard>
+              <Button type="button">Hover to show</Button>
 
-        <Flex width={320} padding>
-          <Text>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
-            architecto qui quis ipsum id pariatur asperiores voluptate eveniet
-            nisi maiores, natus optio, culpa, possimus reprehenderit doloribus
-            placeat in illum harum.
-          </Text>
-        </Flex>
-      </HoverCard>
+              <Flex padding>HoverCard content</Flex>
+            </HoverCard>
+          </Flex>
+        </Card>
+
+        <Spacer height />
+        <Heading>Anatomy</Heading>
+        <CodeHighlight>{exampleCode}</CodeHighlight>
+
+        <Spacer height />
+        <Heading>API reference</Heading>
+        <Heading size="md">HoverCard</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem required>children[]</SimpleTableItem>
+            <SimpleTableItem>ReactNode</SimpleTableItem>
+            <SimpleTableItem />
+          </SimpleTableBody>
+        </SimpleTable>
+      </Flex>
     </Flex>
   );
 }
--- src/pages/Overlay/PopoverPage.tsx
+++ src/pages/Overlay/PopoverPage.tsx
@@ -1,19 +1,64 @@
 import React from 'react';
 
 import { Button } from '../../components/Button';
+import { Card } from '../../components/Card';
+import { CodeHighlight } from '../../components/CodeHighlight';
 import { Flex } from '../../components/Flex';
+import { Heading } from '../../components/Heading';
 import { Popover } from '../../components/Popover';
+import {
+  SimpleTable,
+  SimpleTableHeader,
+  SimpleTableItem,
+} from '../../components/SimpleTable';
+import { SimpleTableBody } from '../../components/SimpleTable/SimpleTableBody';
+import { Spacer } from '../../components/Spacer';
+
+const exampleCode = `import { Popover } from '@siakit';
+
+<Popover>
+  /* trigger */
+
+  /* content */
+</Popover>`;
 
 export function PopoverPage(): JSX.Element {
   return (
-    <Flex flex justify="center" align="center">
-      <Popover>
-        <Button type="button">Click to popover</Button>
+    <Flex flex direction="column" align="center" overflow>
+      <Flex width={768} padding={32} direction="column" gap>
+        <Heading size="xl">Popover</Heading>
+        <Card>
+          <Flex height={192} padding justify="center" align="center">
+            <Popover>
+              <Button type="button">Click to Popover</Button>
 
-        <Flex width={240} padding>
-          <p>test popover content</p>
-        </Flex>
-      </Popover>
+              <Flex padding>Popover content</Flex>
+            </Popover>
+          </Flex>
+        </Card>
+
+        <Spacer height />
+        <Heading>Anatomy</Heading>
+        <CodeHighlight>{exampleCode}</CodeHighlight>
+
+        <Spacer height />
+        <Heading>API reference</Heading>
+        <Heading size="md">Popover</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem required>children[]</SimpleTableItem>
+            <SimpleTableItem>ReactNode</SimpleTableItem>
+            <SimpleTableItem />
+          </SimpleTableBody>
+        </SimpleTable>
+      </Flex>
     </Flex>
   );
 }
