90a9b463a6531cf6a4e7b85af21801d490da0b37
= <=>
2022-06-29 11:55:58 -0300

    progressbar docs

--- src/pages/Feedback/ProgressBarPage.tsx
+++ src/pages/Feedback/ProgressBarPage.tsx
@@ -1,54 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { Button } from '../../components/Button';
+import { Card } from '../../components/Card';
+import { CodeHighlight } from '../../components/CodeHighlight';
 import { Flex } from '../../components/Flex';
+import { Heading } from '../../components/Heading';
 import { ProgressBar } from '../../components/ProgressBar';
+import {
+  SimpleTable,
+  SimpleTableHeader,
+  SimpleTableItem,
+} from '../../components/SimpleTable';
+import { SimpleTableBody } from '../../components/SimpleTable/SimpleTableBody';
+import { Spacer } from '../../components/Spacer';
+
+const exampleCode = `import { ProgressBar } from '@siakit';
+
+<ProgressBar />`;
 
 export function ProgressBarPage(): JSX.Element {
-  const [progress, setProgress] = useState(0);
-
   return (
-    <Flex flex justify="center" align="center" direction="column" gap={64}>
-      <Flex gap={8}>
-        <Button type="button" colorScheme="pink">
-          qualquer coisa
-        </Button>
-        <Button type="button" onClick={() => setProgress(0)} colorScheme="pink">
-          to 0%
-        </Button>
-        <Button
-          type="button"
-          onClick={() => setProgress(25)}
-          colorScheme="pink"
-        >
-          to 25%
-        </Button>
-        <Button
-          type="button"
-          onClick={() => setProgress(50)}
-          colorScheme="pink"
-        >
-          to 50%
-        </Button>
-        <Button
-          type="button"
-          onClick={() => setProgress(75)}
-          colorScheme="pink"
-        >
-          to 75%
-        </Button>
-        <Button
-          type="button"
-          onClick={() => setProgress(100)}
-          colorScheme="pink"
-        >
-          to 100%
-        </Button>
-      </Flex>
+    <Flex flex direction="column" align="center" overflow>
+      <Flex width={768} padding={32} direction="column" gap>
+        <Heading size="xl">ProgressBar</Heading>
+        <Card>
+          <Flex height={192} padding justify="center" align="center">
+            <Flex flex width="75%">
+              <ProgressBar progress={37} showPercentage />
+            </Flex>
+          </Flex>
+        </Card>
 
-      <Flex width={440} direction="column" gap>
-        <ProgressBar progress={progress} />
-        {/* <ProgressBar progress={progress} showPercentage /> */}
+        <Spacer height />
+        <Heading>Anatomy</Heading>
+        <CodeHighlight>{exampleCode}</CodeHighlight>
+
+        <Spacer height />
+        <Heading>API reference</Heading>
+        <Heading size="md">ProgressBar</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem required>progress</SimpleTableItem>
+            <SimpleTableItem>number</SimpleTableItem>
+            <SimpleTableItem />
+
+            <SimpleTableItem>showPercentage</SimpleTableItem>
+            <SimpleTableItem>boolean</SimpleTableItem>
+            <SimpleTableItem>false</SimpleTableItem>
+          </SimpleTableBody>
+        </SimpleTable>
       </Flex>
     </Flex>
   );
