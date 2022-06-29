1e443ce31eb7abde712db227e2d62f4684aee188
= <=>
2022-06-29 12:01:17 -0300

    shimmer docs

--- src/pages/Feedback/ShimmerPage.tsx
+++ src/pages/Feedback/ShimmerPage.tsx
@@ -1,15 +1,83 @@
 import React from 'react';
 
+import { Card } from '../../components/Card';
+import { CodeHighlight } from '../../components/CodeHighlight';
 import { Flex } from '../../components/Flex';
-import { Rectangle, Ellipse } from '../../components/Shimmer';
+import { Heading } from '../../components/Heading';
+import { ProgressBar } from '../../components/ProgressBar';
+import { Ellipse, Rectangle } from '../../components/Shimmer';
+import {
+  SimpleTable,
+  SimpleTableHeader,
+  SimpleTableItem,
+} from '../../components/SimpleTable';
+import { SimpleTableBody } from '../../components/SimpleTable/SimpleTableBody';
+import { Spacer } from '../../components/Spacer';
+
+const exampleCode = `import { Rectangle, Ellipse } from '@siakit';
+
+<Rectangle />
+<Ellipse />`;
 
 export function ShimmerPage(): JSX.Element {
   return (
-    <Flex flex justify="center" align="center">
-      <Flex gap={64}>
-        <Rectangle width={128} height={128} />
+    <Flex flex direction="column" align="center" overflow>
+      <Flex width={768} padding={32} direction="column" gap>
+        <Heading size="xl">Shimmer</Heading>
+        <Card>
+          <Flex height={192} padding justify="center" align="center" gap>
+            <Rectangle width={128} height={128} />
 
-        <Ellipse width={128} height={128} />
+            <Ellipse width={128} height={128} />
+          </Flex>
+        </Card>
+
+        <Spacer height />
+        <Heading>Anatomy</Heading>
+        <CodeHighlight>{exampleCode}</CodeHighlight>
+
+        <Spacer height />
+        <Heading>API reference</Heading>
+        <Heading size="md">Rectangle</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem>width</SimpleTableItem>
+            <SimpleTableItem>number | string</SimpleTableItem>
+            <SimpleTableItem />
+
+            <SimpleTableItem>height</SimpleTableItem>
+            <SimpleTableItem>number | string</SimpleTableItem>
+            <SimpleTableItem />
+          </SimpleTableBody>
+        </SimpleTable>
+
+        <Spacer height />
+        <Heading size="md">Ellipse</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem>width</SimpleTableItem>
+            <SimpleTableItem>number | string</SimpleTableItem>
+            <SimpleTableItem />
+
+            <SimpleTableItem>height</SimpleTableItem>
+            <SimpleTableItem>number | string</SimpleTableItem>
+            <SimpleTableItem />
+          </SimpleTableBody>
+        </SimpleTable>
       </Flex>
     </Flex>
   );
