9544bec506222bed87333c79cbc0db127cdbdaeb
= <=>
2022-06-30 08:32:45 -0300

    add avatar docs

--- src/pages/Media/AvatarPage.tsx
+++ src/pages/Media/AvatarPage.tsx
@@ -1,27 +1,75 @@
 import React from 'react';
 
 import { Avatar } from '../../components/Avatar';
+import { Button } from '../../components/Button';
+import { Card } from '../../components/Card';
+import { CodeHighlight } from '../../components/CodeHighlight';
 import { Flex } from '../../components/Flex';
+import { Heading } from '../../components/Heading';
+import { LinkButton } from '../../components/LinkButton';
+import {
+  SimpleTable,
+  SimpleTableHeader,
+  SimpleTableItem,
+} from '../../components/SimpleTable';
+import { SimpleTableBody } from '../../components/SimpleTable/SimpleTableBody';
+import { Spacer } from '../../components/Spacer';
+
+const exampleCode = `import { Avatar } from '@siakit';
+
+<Avatar />`;
 
 export function AvatarPage(): JSX.Element {
   return (
-    <Flex flex justify="center" align="center" gap={32}>
-      <Flex gap>
-        <Avatar src="https://github.com/brunohfabre.png" size="xs" badge />
-        <Avatar src="https://github.com/brunohfabre.png" size="sm" badge />
-        <Avatar src="https://github.com/brunohfabre.png" size="md" badge />
-        <Avatar src="https://github.com/brunohfabre.png" size="lg" badge />
-        <Avatar src="https://github.com/brunohfabre.png" size="xl" badge />
-        <Avatar src="https://github.com/brunohfabre.png" size="2xl" badge />
-      </Flex>
+    <Flex flex direction="column" align="center" overflow>
+      <Flex width={768} padding={32} direction="column" gap>
+        <Heading size="xl">Avatar</Heading>
+        <Card>
+          <Flex height={192} padding justify="center" align="center" gap>
+            <Avatar
+              name="John Doe"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHNob3R8ZW58MHx8MHx8&w=1000&q=80"
+            />
+            <Avatar name="John Doe" />
+            <Avatar name="John Doe" badge />
+          </Flex>
+        </Card>
 
-      <Flex gap>
-        <Avatar size="xs" name="Bruno Fabre" />
-        <Avatar size="sm" name="Bruno henrique de Fabre" />
-        <Avatar size="md" name="Bruno Fabre" />
-        <Avatar size="lg" name="Bruno Fabre" />
-        <Avatar size="xl" name="Bruno Fabre" />
-        <Avatar size="2xl" name="Bruno Fabre" badge />
+        <Spacer height />
+        <Heading>Anatomy</Heading>
+        <CodeHighlight>{exampleCode}</CodeHighlight>
+
+        <Spacer height />
+        <Heading>API reference</Heading>
+        <Heading size="md">Button</Heading>
+
+        <SimpleTable>
+          <SimpleTableHeader>
+            <SimpleTableItem isHeader>Prop</SimpleTableItem>
+            <SimpleTableItem isHeader>Type</SimpleTableItem>
+            <SimpleTableItem isHeader>Default</SimpleTableItem>
+          </SimpleTableHeader>
+
+          <SimpleTableBody>
+            <SimpleTableItem>name (required if not src prop)</SimpleTableItem>
+            <SimpleTableItem>string</SimpleTableItem>
+            <SimpleTableItem />
+
+            <SimpleTableItem>src (required if not name prop)</SimpleTableItem>
+            <SimpleTableItem>string</SimpleTableItem>
+            <SimpleTableItem />
+
+            <SimpleTableItem>size</SimpleTableItem>
+            <SimpleTableItem
+              isEnum
+            >{`"xs" | "sm" | "md" | "lg" | "xl" | "2xl"`}</SimpleTableItem>
+            <SimpleTableItem>md</SimpleTableItem>
+
+            <SimpleTableItem>badge</SimpleTableItem>
+            <SimpleTableItem>boolean</SimpleTableItem>
+            <SimpleTableItem>false</SimpleTableItem>
+          </SimpleTableBody>
+        </SimpleTable>
       </Flex>
     </Flex>
   );
